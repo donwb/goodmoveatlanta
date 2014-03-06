f6b2a54f61711abefdaf7c526c41e6135dac8257
don bronwing <don.browning@gmail.com>
2014-03-05 22:17:26 -0500

    added config

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
+var config = require('./config');
 
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+//app.set('port', process.env.PORT || 3000);
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -14,15 +15,21 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
+var port;
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
+  port = config.development.EnvConfig.port;
+} else {
+  port = config.production.EnvConfig.port;
 }
 
 app.get('/', function(req, res){
   res.sendfile('public/index.html');
 });
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+console.log('config ' + port);
+
+http.createServer(app).listen(port, function(){
+  console.log('Express server listening on port ' + port);
 });
---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    development: {
+        EnvConfig: {
+            port: 3000
+            }
+        },
+    production: {
+        EnvConfig: {
+            port: 3003
+            }
+        }
+    };
